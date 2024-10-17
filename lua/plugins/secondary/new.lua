55e5b14bde197cc6a4bdb77dadd6350e8c6e05e1
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-10-18 01:08:30 +0200

    .

--- lua/plugins/secondary/new.lua
+++ lua/plugins/secondary/new.lua
@@ -8,6 +8,121 @@ local spec = {
       "nvim-treesitter/nvim-treesitter",
     },
   },
+  {
+    "jellydn/hurl.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = "hurl",
+    cmd = {
+      "HurlJson",
+      "HurlRunner",
+      "HurlRunnerAt",
+      "HurlRunnerToEntry",
+      "HurlShowLastResponse",
+      "HurlToggleMode",
+      "HurlVerbose",
+    },
+    keys = require("mappings").hurl,
+    config = function()
+      require("hurl").setup({
+        auto_close = false,
+        show_notification = true,
+        mode = "popup",
+        popup_position = "50%",
+        popup_size = {
+          width = "90%",
+          height = "90%",
+        },
+        mappings = {
+          close = "<c-c>",
+        },
+      })
+    end,
+  },
+  {
+    "jellydn/hurl.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    ft = "hurl",
+    cmd = {
+      "HurlJson",
+      "HurlRunner",
+      "HurlRunnerAt",
+      "HurlRunnerToEntry",
+      "HurlShowLastResponse",
+      "HurlToggleMode",
+      "HurlVerbose",
+    },
+    keys = require("mappings").hurl,
+    config = function()
+      require("hurl").setup({
+        auto_close = false,
+        show_notification = true,
+        mode = "popup",
+        popup_position = "50%",
+        popup_size = {
+          width = "90%",
+          height = "90%",
+        },
+        mappings = {
+          close = "<c-c>",
+        },
+      })
+    end,
+  },
+
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     -- add any opts here
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
 
 return spec
