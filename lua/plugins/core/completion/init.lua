ee1b43d3c961c1793cb0c0aa8be79a51c9227db7
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-11-03 03:46:44 +0100

    .

--- lua/plugins/core/completion/init.lua
+++ lua/plugins/core/completion/init.lua
@@ -222,11 +222,11 @@ local spec = {
       "hrsh7th/cmp-path",
     },
     -- use a release tag to download pre-built binaries
-    --version = "v0.*",
+    version = "v0.5.1",
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
-    build = "nix run .#build-plugin",
+    --build = "nix run .#build-plugin",
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
