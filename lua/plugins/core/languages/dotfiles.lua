b9d491f6521f42c1e390f33b06c4d87e91697026
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-11-03 03:33:19 +0100

    .

--- lua/plugins/core/languages/dotfiles.lua
+++ lua/plugins/core/languages/dotfiles.lua
@@ -34,13 +34,44 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "git_config",
-        "rasi",
-        "dockerfile",
-      },
-    },
+    opts = function(_, opts)
+      local function have(path)
+        return vim.uv.fs_stat(xdg_config .. "/" .. path) ~= nil
+      end
+      local function add(lang)
+        if type(opts.ensure_installed) == "table" then
+          table.insert(opts.ensure_installed, lang)
+        end
+      end
+
+      vim.filetype.add({
+        extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+        filename = {
+          ["vifmrc"] = "vim",
+        },
+        pattern = {
+          [".*/waybar/config"] = "jsonc",
+          [".*/mako/config"] = "dosini",
+          [".*/kitty/.+%.conf"] = "bash",
+          [".*/hypr/.+%.conf"] = "hyprlang",
+          ["%.env%.[%w_.-]+"] = "sh",
+        },
+      })
+
+      add("git_config")
+
+      if have("hypr") then
+        add("hyprlang")
+      end
+
+      if have("fish") then
+        add("fish")
+      end
+
+      if have("rofi") or have("wofi") then
+        add("rasi")
+      end
+    end,
   },
   {
     "nvimtools/none-ls.nvim",
--- lua/plugins/secondary/editor.lua
+++ lua/plugins/secondary/editor.lua
@@ -81,6 +81,113 @@ local spec = {
     end,
   },
   {
+    "mistweaverco/kulala.nvim",
+    ft = "http",
+    keys = {
+      { "<leader>R", "", desc = "+Rest" },
+      { "<leader>Rs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request" },
+      { "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body" },
+      { "<leader>Rp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request" },
+      { "<leader>Rn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request" },
+    },
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "http", "graphql" },
+    },
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    recommended = true,
+    desc = "Highlight colors in your code. Also includes Tailwind CSS support.",
+    event = "LazyFile",
+    opts = function()
+      local hi = require("mini.hipatterns")
+      return {
+        -- custom LazyVim option to enable the tailwind integration
+        tailwind = {
+          enabled = true,
+          ft = {
+            "astro",
+            "css",
+            "heex",
+            "html",
+            "html-eex",
+            "javascript",
+            "javascriptreact",
+            "rust",
+            "svelte",
+            "typescript",
+            "typescriptreact",
+            "vue",
+          },
+          -- full: the whole css class will be highlighted
+          -- compact: only the color will be highlighted
+          style = "full",
+        },
+        highlighters = {
+          hex_color = hi.gen_highlighter.hex_color({ priority = 2000 }),
+          shorthand = {
+            pattern = "()#%x%x%x()%f[^%x%w]",
+            group = function(_, _, data)
+              ---@type string
+              local match = data.full_match
+              local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+              local hex_color = "#" .. r .. r .. g .. g .. b .. b
+
+              return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+            end,
+            extmark_opts = { priority = 2000 },
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      if type(opts.tailwind) == "table" and opts.tailwind.enabled then
+        -- reset hl groups when colorscheme changes
+        vim.api.nvim_create_autocmd("ColorScheme", {
+          callback = function()
+            M.hl = {}
+          end,
+        })
+        opts.highlighters.tailwind = {
+          pattern = function()
+            if not vim.tbl_contains(opts.tailwind.ft, vim.bo.filetype) then
+              return
+            end
+            if opts.tailwind.style == "full" then
+              return "%f[%w:-]()[%w:-]+%-[a-z%-]+%-%d+()%f[^%w:-]"
+            elseif opts.tailwind.style == "compact" then
+              return "%f[%w:-][%w:-]+%-()[a-z%-]+%-%d+()%f[^%w:-]"
+            end
+          end,
+          group = function(_, _, m)
+            ---@type string
+            local match = m.full_match
+            ---@type string, number
+            local color, shade = match:match("[%w-]+%-([a-z%-]+)%-(%d+)")
+            shade = tonumber(shade)
+            local bg = vim.tbl_get(M.colors, color, shade)
+            if bg then
+              local hl = "MiniHipatternsTailwind" .. color .. shade
+              if not M.hl[hl] then
+                M.hl[hl] = true
+                local bg_shade = shade == 500 and 950 or shade < 500 and 900 or 100
+                local fg = vim.tbl_get(M.colors, color, bg_shade)
+                vim.api.nvim_set_hl(0, hl, { bg = "#" .. bg, fg = "#" .. fg })
+              end
+              return hl
+            end
+          end,
+          extmark_opts = { priority = 2000 },
+        }
+      end
+      require("mini.hipatterns").setup(opts)
+    end,
+  },
+  {
     "gbprod/cutlass.nvim",
     event = "KindaLazy",
     config = function(_, opts)
