3030685e9e5d5eb8af1d814b2227f7c41fc2367b
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-11-03 03:42:35 +0100

    .

--- lua/plugins/core/completion/init.lua
+++ lua/plugins/core/completion/init.lua
@@ -214,8 +214,13 @@ local spec = {
     "saghen/blink.cmp",
     lazy = false, -- lazy loading handled internally
     -- optional: provides snippets for the snippet source
-    dependencies = "rafamadriz/friendly-snippets",
-
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
     -- use a release tag to download pre-built binaries
     --version = "v0.*",
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
