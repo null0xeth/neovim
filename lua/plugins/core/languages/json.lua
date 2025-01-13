4c69f6e3a735db07f85681ce48074ee307b576bd
null0xeth <112922737+null0xeth@users.noreply.github.com>
2025-01-13 23:20:37 +0100

    .die

--- lua/plugins/core/languages/json.lua
+++ lua/plugins/core/languages/json.lua
@@ -1,25 +1,47 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "jq",
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, { json = { "prettierd" } })
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "jq",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = function(_, opts)
+  --     opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, { json = { "prettierd" } })
+  --   end,
+  -- },
   {
     "mfussenegger/nvim-lint",
     opts = function(_, opts)
       opts.linters_by_ft["json"] = { "jsonlint" }
     end,
   },
-
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "jq",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["jsonc"] = { "deno_fmt" },
+        ["json"] = { "deno_fmt", "prettierd" },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "json", "jsonc" },
+    },
+  },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
